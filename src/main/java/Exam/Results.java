26f2f2ef03921e44457299742e70c65a676a0e7f
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-09-11 15:15:37 +0530

    basics stuffs

---
+++ src/main/java/Exam/Results.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exam;
+
+/**
+ *
+ * @author Bhathiya Prasad.
+ */
+public class Results {
+    Results () {
+        System.out.println("Print its result time");
+        
+    }
+    
+    public void selected(int x,  int y){
+        System.out.println("results are good so you are selected");
+    }
+}
+ 
